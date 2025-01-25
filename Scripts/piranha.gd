95acf6c6ed0b487daee075508410de2d63496e6a
oceanesq <oceane.copub@gmail.com>
2025-01-25 14:45:50 +0100

    merge test

--- Scripts/piranha.gd
+++ Scripts/piranha.gd
@@ -2,28 +2,19 @@ extends CharacterBody2D
 
 
 const SPEED = 200.0
-const JUMP_VELOCITY = -400.0
 var direction = 1 #Start to the right
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction := Input.get_axis("ui_left", "ui_right")
-
+	movement()
+	move_and_slide()
+	
+func movement():
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
 	
-
-	
-
-func _on_animated_sprite_2d_animation_changed() -> void:
-	direction *= -1  # Inverse direction
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	direction *= -1
