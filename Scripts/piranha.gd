20da34f8b6adfc2e65e636cb462b9a347831ccdb
oceanesq <oceane.copub@gmail.com>
2025-01-25 14:56:38 +0100

    try merge

--- Scripts/piranha.gd
+++ Scripts/piranha.gd
@@ -4,17 +4,17 @@ extends CharacterBody2D
 const SPEED = 200.0
 var direction = 1 #Start to the right
 
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	direction *= -1
+	
+func movement():
+	velocity.x = direction * SPEED
+
 
 func _physics_process(delta: float) -> void:
 	movement()
 	move_and_slide()
-	
-func movement():
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	direction *= -1
+
+func _on_animated_sprite_2d_animation_changed() -> void:
+	pass # Replace with function body.
