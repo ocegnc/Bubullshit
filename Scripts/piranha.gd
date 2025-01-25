79db90802222b76a45387ffdf0466635d144ae77
oceanesq <oceane.copub@gmail.com>
2025-01-25 13:29:30 +0100

    piranha change direction when collision

--- Scripts/piranha.gd
+++ Scripts/piranha.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
+var direction = 1 #Start to the right
 
 
 func _physics_process(delta: float) -> void:
@@ -13,10 +14,13 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	#var direction := Input.get_axis("ui_left", "ui_right")
-	const direction = 1
+
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	if get_slide_collision_count()>0:  
+			direction *= -1  # Inverse direction
