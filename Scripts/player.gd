9ee46ade907b4c6dc72b4b915fc11a0f7d8124cf
ocegnc <oceane.guennec@gmail.com>
2025-01-26 10:49:12 +0100

    hearts

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,9 +1,18 @@
 extends CharacterBody2D
 
 var health = 3
+const MAX_HEALTH = 3
+
 const POSITION = 1000
 var bubble_path = preload("res://Scenes/sample_scene.tscn")
 var bubble
+
+#Called heart container
+@onready var heart_container = get_parent().get_node("CanvasLayer/HBoxContainer")
+
+#invicibility during 1s after collision
+const INVINCIBILITY_TIME = 1
+var is_invincible = false 
 
 # Threshold for activating the fart mechanic
 const FART_THRESHOLD = 0.8
@@ -24,10 +33,38 @@ func fart() -> void:
 	bubble.pos = global_position
 	bubble.rota = global_rotation
 	get_parent().add_child(bubble)
+
+func life_lost() -> void:
+	if not is_invincible:
+		health -= 1
+		update_hearts()
+		if health <= 0:
+			game_over()
+		else:
+			become_invincible()
+
+#player becomes temporarely invicible after collision
+func become_invincible() -> void:
+	is_invincible = true
+	await get_tree().create_timer(INVINCIBILITY_TIME).timeout
+	is_invincible = false
+
+#Change life with a broken_heart
+func update_hearts() -> void:
+	for i in range(MAX_HEALTH):
+		var heart = heart_container.get_child(i)
+		if i < health:
+			heart.texture = preload("res://Assets/heart_icon.png")
+		else:
+			heart.texture = preload("res://Assets/broken_heart_icon.png")
 	
 
-
 func _on_area_2d_body_entered(body):
-	if body.is_in_group("piranha"):  
+	if body.is_in_group("piranha") and not is_invincible:  
 		body.reset_position()
-		health = health -1
+		life_lost()
+
+func game_over() -> void:
+	get_parent().get_node("CanvasLayer/GameOverScreen").visible = true
+	await get_tree().create_timer(2.0).timeout
+	get_tree().reload_current_scene()
